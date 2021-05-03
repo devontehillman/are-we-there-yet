7a2c3e824d8124f2ddfb2e07b269963e4ab78458
Andrew Chappell <chappand@gmail.com>
2021-05-03 19:53:01 -0400

    Added API routes for axios.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,2 +1,20 @@
+import axios from "axios";
 
-
+export default {
+    // Gets all books
+    getTopics: function() {
+      return axios.get("/api/topics");
+    },
+    // Gets the book with the given id
+    getTopic: function(id) {
+      return axios.get("/api/topic/" + id);
+    },
+    // Deletes the book with the given id
+    deleteTopic: function(id) {
+      return axios.delete("/api/topics/" + id);
+    },
+    // Saves a book to the database
+    saveTopic: function(topicData) {
+      return axios.post("/api/topics", topicData);
+    }
+  };

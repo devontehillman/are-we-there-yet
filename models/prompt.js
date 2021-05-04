1ab8eb6c9f4238468284c91d404e6fd8ac60f28c
Andrew Chappell <chappand@gmail.com>
2021-05-03 20:50:55 -0400

    Added user and response models.

--- models/prompt.js
+++ models/prompt.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const promptSchema = new Prompt({
+const promptSchema = new Schema({
   userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   topic: { type: String, required: true },
   questionType: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  responses: { type: String, required: false },
+  date: { type: Date, default: Date.now }
 });
 
 const Prompt = mongoose.model("Prompt", promptSchema);
--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const promptSchema = new Schema({
-  userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  topic: { type: String, required: true },
-  promptType: { type: String, required: true },
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  userName: { type: String, required: true},
+  password: {type: String, required: true},
+  userType: {type: String, required: true},
   date: { type: Date, default: Date.now },
-  responses: { type: mongoose.Schema.Types.String, ref: "responses" },
 });
 
-const Prompt = mongoose.model("Prompt", promptSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = Prompt;
+module.exports = User;

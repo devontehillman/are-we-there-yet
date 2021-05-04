8f5eacd777c632a3c6f4a861866012fb50f7f05f
colemilwee <rocolebot@gmail.com>
2021-05-03 20:52:04 -0400

    added more to SignUp

--- src/Components/SignUp/index.js
+++ src/Components/SignUp/index.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form'
+import React, {Component} from 'react'
 
-function SignUp() {
+class SignUp extends Component {
+    constructor(){
+        super();
+        this.state={
+            username:'',
+            password:'',
+        }
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        fetch ('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
+render() {
     return (
         <div>
-            <Form>
+        <form onSubmit={this.onSubmit} className="login">
                 <div action="/action_page.php" class="signup">
                     <label  htmlFor="Fname" onChange={this.handleChange} >
                         First name:
@@ -33,9 +63,10 @@ function SignUp() {
                     <input type="text" name="email"  value={this.state.value} placeholder="Email" />
                     <input type="submit" value="Submit" />
                 </div>
-            </Form>
+                </form>
         </div>
     )
+}
 }
 
 export default SignUp;

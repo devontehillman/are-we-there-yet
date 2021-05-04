e80516ef0de18ec154349f1c96c5914eb275a4ab
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-05-03 20:56:20 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Render } from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
 import NavBar from "./Components/Navbar"
-import Welcome from "./Components/Welcome"
+
+import Welcome from "./pages/welcome"
 import Login from "./pages/login"
+import SignUp from "./pages/signup"
 
 function App (){
     return(
-        <div>
-        <NavBar />
-        <Welcome />
-        </div>
-    //     <Router>
-           
-    // <div>
-    // <NavBar />
-    // <Switch>
-    //         <Route path="/" exact component={Welcome}/>
-    //          <Route path="/login" exact component={Login}/>
-    // </Switch>
-    // </div>
-    // </Router>
+    <Router>
+    <div>
+    <NavBar />
+    <Switch>
+        <Route path="/" exact component={Welcome}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/signup" exact component={SignUp}/>
+    </Switch>
+    </div>
+    </Router>
     )
 }
 

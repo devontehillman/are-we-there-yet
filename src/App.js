0d401d39acd8e4b3076a649abc1e1128ba725b39
colemilwee <rocolebot@gmail.com>
2021-05-03 22:58:37 -0400

    fixed routing error

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import { Render } from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import NavBar from './Components/Navbar'
-import Welcome from './Components/Welcome'
-import SignUp from './Components/signUp'
-
+import NavBar from "./Components/Navbar"
+import Welcome from "./pages/welcome"
+import Login from "./pages/login"
+import SignUp from "./pages/signup"
 function App (){
     return(
-        <div>
-        <NavBar />
-        <Welcome />
-        <SignUp />
-        </div>
-        <Router>
-           
-     <div>
-     <NavBar />
+    <Router>
+    <div>
+    <NavBar />
     <Switch>
-             <Route path="/" exact component={Welcome}/>
-          <Route path="/login" exact component={Login}/>
-     </Switch>
-    
-     </Router>
-     </div>
+        <Route path="/" exact component={Welcome}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/signup" exact component={SignUp}/>
+    </Switch>
+    </div>
+    </Router>
     )
 }
-
 export default App;

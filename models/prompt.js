38e7678cc9b79626425bfacb261e7830c321bba4
Andrew Chappell <chappand@gmail.com>
2021-05-08 11:49:06 -0400

    Got the Database Connected and Seeded

--- models/prompt.js
+++ models/prompt.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+import Response from "./response";
 
 const promptSchema = new Schema({
   userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   topic: { type: String, required: true },
   questionType: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  responses: [Response]
 });
 
 const Prompt = mongoose.model("Prompt", promptSchema);
--- models/response.js
+++ models/response.js
@@ -5,7 +5,7 @@ const responseSchema = new Schema({
   userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   answer: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  promptID: { type: mongoose.Schema.Types.ObjectId, ref: "Prompt" },
+  // promptID: { type: mongoose.Schema.Types.ObjectId, ref: "Prompt" },
 });
 
 const Response = mongoose.model("Prompt", responseSchema);

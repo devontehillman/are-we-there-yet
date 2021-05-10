3e0af71c2ef78f9738b8b8cc4240a10d187ee5f4
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-05-10 19:16:28 -0400

    styling for Nav

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
+import NavBar from "react-bootstrap/Navbar"
 
 class Navbar extends Component {
   render() {
     return (
-      <Nav>
-      <Container>
-      <div className="navbar-header">
-           <img src="images/AWtY.png" alt="Are We There Yet logo" style= {{width: "200px", height: "100px"}} />
-         </div>
-         <ul className="nav navbar-nav">
-           <li className="active"><a href="./">Home</a></li>
-         </ul>
-         <Nav.Link href="./register" class="sign-login"><span class="glyphicon glyphicon-user"></span> Sign Up</Nav.Link>
-           <Nav.Link href="./login" class="sign-login"><span class="glyphicon glyphicon-log-in"></span> Login</Nav.Link>
-    
-         </Container>
-       </Nav>
+      <div>
+        <NavBar className="NavBar">
+          <Container>
+            <NavBar.Brand href="#home" className="cornerlogo"><img src="images/AWtY.png" alt="are we there yet logo" className="cornerlogo"></img></NavBar.Brand>
+            <Nav className="selection">
+              <Nav.Link href="./"><span className="glyphicon glyphicon-home"></span>Home</Nav.Link>
+              <Nav.Link href="./Register"><span className="glyphicon glyphicon-user"></span>Sign Up</Nav.Link>
+              <Nav.Link href="./Login"><span className="glyphicon glyphicon-log-in"></span>Login</Nav.Link>
+            </Nav>
+          </Container>
+        </NavBar>
+      </div>
     );
   }
 }

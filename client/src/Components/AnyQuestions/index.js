bbd3e42969136292a74c1fa894f356f938d8f0ec
Andrew Pieschke <ampieschke@gmail.com>
2021-05-15 10:41:38 -0400

    added onsubmit functions

--- client/src/Components/AnyQuestions/index.js
+++ client/src/Components/AnyQuestions/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import API from "../../utils/API";
 
-function AnyQuestions(props) {
-return (
+function AnyQuestions({ topic }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Hi, good job!");
+    console.log(e.target.value);
+    const resp = e.target.value;
+    API.saveResponse({
+      userID: "6099bf2843afca3ed4f46e53",
+      topicID: { topic },
+      answer: resp,
+    });
+  }
+
+  return (
     <div className="row">
-        <input placeholder="type question here"></input>
+      <input placeholder="type question here"></input>
+      <button onClick={handleSubmit} value="up">
+        {" "}
+      </button>
     </div>
-)
+  );
 }
 
 export default AnyQuestions;
--- client/src/Components/MultiButton/index.js
+++ client/src/Components/MultiButton/index.js
@@ -1,15 +1,37 @@
 import React from "react";
+import API from "../../utils/API";
 
-function MultiButton(props) {
-return (
+function MultiButton({ topic }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Hi, good job!");
+    console.log(e.target.value);
+    const resp = e.target.value;
+    API.saveResponse({
+      userID: "6099bf2843afca3ed4f46e53",
+      topicID: { topic },
+      answer: resp,
+    });
+  }
+  return (
     <div className="row">
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>5</button>
+      <button onClick={handleSubmit} value="1">
+        1
+      </button>
+      <button onClick={handleSubmit} value="2">
+        2
+      </button>
+      <button onClick={handleSubmit} value="3">
+        3
+      </button>
+      <button onClick={handleSubmit} value="4">
+        4
+      </button>
+      <button onClick={handleSubmit} value="5">
+        5
+      </button>
     </div>
-)
+  );
 }
 
 export default MultiButton;

a6347067f3211efafc8916657a2eaf6250e457c8
devontehillman <hillmand@mail.gvsu.edu>
2021-05-18 01:26:43 -0400

    Removing cached files

---
+++ client/build/precache-manifest.1dfc399852e9adb3c3a6f5c68ebc7b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "630802921c77310a0490",
+    "url": "/static/css/main.ccb8f474.chunk.css"
+  },
+  {
+    "revision": "630802921c77310a0490",
+    "url": "/static/js/main.63080292.chunk.js"
+  },
+  {
+    "revision": "25a7b5167cbacd4c9907",
+    "url": "/static/css/1.f279312d.chunk.css"
+  },
+  {
+    "revision": "25a7b5167cbacd4c9907",
+    "url": "/static/js/1.25a7b516.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32ed566bb595fbcffead1db16836768e",
+    "url": "/index.html"
+  }
+];

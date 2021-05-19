245f9b266244a18e4f83f0ead6a78a5234d3de91
devontehillman <hillmand@mail.gvsu.edu>
2021-05-19 15:18:39 -0400

    resorting input feild functionality

--- client/src/Components/layout/Navbar.js
+++ client/src/Components/layout/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
               {/* <Nav.Link href="./Register"><span className="glyphicon glyphicon-user"></span>Sign Up</Nav.Link>
               <Nav.Link href="./Login"><span className="glyphicon glyphicon-log-in"></span>Login</Nav.Link> */}
               <Nav.Link href="./addtopic"><span className="glyphicon glyphicon-log-in"></span>Add Topic</Nav.Link>
-              <Nav.Link href="./viewtopics"><span className="glyphicon glyphicon-log-in"></span>See Topics</Nav.Link>
+              <Nav.Link href="./viewtopics"><span className="glyphicon glyphicon-log-in"></span>Answer Prompt</Nav.Link>
             </Nav>
           </Container>
         </NavBar>

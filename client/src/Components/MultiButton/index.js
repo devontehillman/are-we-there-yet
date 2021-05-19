a2c73b9ee4a62f71a23748952ac5ed455c487525
devontehillman <hillmand@mail.gvsu.edu>
2021-05-18 22:40:38 -0400

    removed .col css and added it to single compontent

--- client/src/Components/MultiButton/index.js
+++ client/src/Components/MultiButton/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import API from "../../utils/API";
-import './../../multibutton.css'
-
 
 function MultiButton({ topic }) {
+  const btnStyle = {
+    margin:"5px"
+  }
+
   let newtopic;
   grabTopic();
   function something(res) {
@@ -33,11 +35,11 @@ function MultiButton({ topic }) {
   return (
     <div className="row" >
        <div className="col s12">
-      <button type="button" class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text green lighten-3" onClick={handleSubmit} value="1">1</button>
-      <button type="button" class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text left green lighten-1" onClick={handleSubmit} value="2">2</button>
-      <button type="button" class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text green left" onClick={handleSubmit} value="3">3</button>
-      <button type="button" class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text green darken-1 left" onClick={handleSubmit} value="4">4</button>
-      <button type="button" class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light left white-text green darken-2" onClick={handleSubmit} value="5">5</button>
+      <button type="button" style={btnStyle} class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text green lighten-3" onClick={handleSubmit} value="1">1</button>
+      <button type="button" style={btnStyle} class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text left green lighten-1" onClick={handleSubmit} value="2">2</button>
+      <button type="button" style={btnStyle} class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text green left" onClick={handleSubmit} value="3">3</button>
+      <button type="button" style={btnStyle} class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light white-text green darken-1 left" onClick={handleSubmit} value="4">4</button>
+      <button type="button" style={btnStyle} class="col xl2 l2 m2 s12 .multi-btn btn-large hoverable waves-effect waves-light left white-text green darken-2" onClick={handleSubmit} value="5">5</button>
       </div>
     </div>
   );
